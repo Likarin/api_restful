85865c9a669652fcdbf4585689fcc6c05c09c8b6
Likarin <reikakannagi@gmail.com>
2018-11-30 10:35:46 +0100

    update server.js

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const fs = require ("fs");
 
+/* method >listUsers
+*/
+
+app.get('/listUsers', function (req, res){
+	fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err,data){
+		console.log(data);
+		red.send(data);
+	});
+})
+
 let server = app.listen(8081, function (){
 	let host = server.address().address
 	let port = server.address().port
 	console.log("Launch API restful with the following URI http://%s:%s", host, port)
 
 })
+
+
+
